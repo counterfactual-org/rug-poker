6f7d29d174f1a78c984ccd6bdeb0f34b5105b43b
LevX <team@levx.io>
2024-08-08 18:15:56 +0900

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 source .env
-forge script DeployScript --rpc-url $RPC_URL --broadcast
+# --skip-simulation for preventing error 'intrinsic gas too low'
+forge script DeployScript --rpc-url $RPC_URL --broadcast --skip-simulation
